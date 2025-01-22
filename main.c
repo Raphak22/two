71234949fcc4b26673ac905f04437e71978420cd
Raphak22 <rivoliraphael@gmail.com>
2025-01-22 17:09:19 +0100

    hello

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+int i = 0;
+
+
+   for ( i = 0; i < 101 ; i++){
+        printf("%d\n", i);
+}
+   for ( i = 0; i < 101 ; i++){
+        printf("%d\n", i);
+
+}
+
+
+}
